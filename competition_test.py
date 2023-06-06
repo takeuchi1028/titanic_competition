e6f340481cab635c5c54749645092e10fc316e38
FullSalt <masaya.mitsushio@kikagaku.co.jp>
2023-06-07 06:51:49 +0900

    fix

--- competition_test.py
+++ competition_test.py
@@ -16,16 +16,22 @@ colunm_name = 'Survived'
 # bank_test[colunm_name] = bank_test[colunm_name].map({'fail': 0, 'success': 1})
 
 st.title('test.csv ファイルに対する評価')
+ranking_df = pd.read_csv('ranking.csv')
+
+st.sidebar.title('評価指標')
+option = ['F1-score', 'Accuracy', 'Recall', 'Precision', 'AUC']
+selected_score = st.sidebar.selectbox('Select your option', option)
+
 
 # streamlitでテキストを入力
 name = st.text_input('名前を入力してください。')
 
 # streamlitでファイルをアップロード
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
 # アップロードしたファイルを読み込み
 if st.button('評価') and uploaded_file is not None:
 
-    ranking_df = pd.read_csv('ranking.csv')
     input_df = pd.read_csv(uploaded_file)
 
     score_acc = round(accuracy_score(bank_test[colunm_name], input_df[colunm_name])*100, 4)
@@ -47,11 +53,30 @@ if st.button('評価') and uploaded_file is not None:
                             'Precision': [score_pre],
                             'F1-score': [score_f1],
                             'AUC': [score_auc]})
-
-    df_score = pd.concat([ranking_df, df_score], axis=0)
-    df_score = df_score.sort_values('F1-score', ascending=False)
-
-    df_score.to_csv('ranking.csv', index=False)
-
-    st.write(df_score)
+    
+    # ranking_df に df_score と同じ行があれば
+    if ranking_df[ranking_df['Name'] == name].empty:
+        ranking_df = pd.concat([ranking_df, df_score], axis=0)
+    
+    ranking_df.to_csv('ranking.csv', index=False)
     # print(df_score)
+
+st.sidebar.download_button('csvファイルを出力', ranking_df.to_csv(index=False), 'ranking.csv')
+cleared_num = st.sidebar.number_input('消去するインデックスを入力', min_value=0)
+if st.sidebar.button('消去'):
+    ranking_df = ranking_df.drop(index=cleared_num)
+    ranking_df.to_csv('ranking.csv', index=False)
+
+ranking_df = ranking_df.sort_values(selected_score, ascending=False)
+rank = ranking_df[selected_score].rank(method='min', ascending=False).astype(int)
+ranking_df.insert(0, 'Rank', rank)
+ranking_df = ranking_df.sort_values('Rank', ascending=True)
+ranking_df = ranking_df.reset_index(drop=True)
+
+st.table(ranking_df)
+
+# # DataFrameをHTMLに変換し、インデックスを非表示にする
+# df_html = ranking_df.to_html(index=False)
+
+# # HTMLを表示
+# st.write(df_html, unsafe_allow_html=True)
